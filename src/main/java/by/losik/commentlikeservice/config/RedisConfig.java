eb7ebd15e65ea170287a26a0584e22edc417ebeb
pYro-sudo <109588143+pYro-sudo@users.noreply.github.com>
2025-10-08 19:52:00 +0300

    Update RedisConfig.java

--- src/main/java/by/losik/commentlikeservice/config/RedisConfig.java
+++ src/main/java/by/losik/commentlikeservice/config/RedisConfig.java
@@ -13,16 +13,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
-    @Value("${spring.redis.password}")
+    @Value("${spring.data.redis.password}")
     private String redisPassword;
 
-    @Value("${spring.redis.database}")
+    @Value("${spring.data.redis.database}")
     private int redisDatabase;
 
     @Bean
@@ -51,4 +51,5 @@ public class RedisConfig {
 
         return new ReactiveRedisTemplate<>(connectionFactory, context);
     }
+
 }
