f076a46303d4d4dfa7634b9b1be6775509c67048
pYro-sudo <losik2006@gmail.com>
2025-10-18 12:14:04 +0300

    really not needed

--- src/main/kotlin/by/losik/activityservice/exception/ValidationException.kt
+++
@@ -1,7 +0,0 @@
-package by.losik.activityservice.exception
-
-class ServiceException(
-    errorCode: String,
-    message: String,
-    cause: Throwable? = null
-) : BaseException(errorCode, message, cause)
