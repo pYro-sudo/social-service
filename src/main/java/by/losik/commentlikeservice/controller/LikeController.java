cd4125cbf3f56d2d1c33493fffa540c60421ffdc
pYro-sudo <109588143+pYro-sudo@users.noreply.github.com>
2025-10-23 15:07:13 +0300

    Update LikeController.java

--- src/main/java/by/losik/commentlikeservice/controller/LikeController.java
+++ src/main/java/by/losik/commentlikeservice/controller/LikeController.java
@@ -17,7 +17,16 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -336,4 +345,5 @@ public class LikeController {
         return likeService.deleteAll()
                 .then(Mono.just(ApiResponse.success("All likes deleted successfully")));
     }
+
 }
